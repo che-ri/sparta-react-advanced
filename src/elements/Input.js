06389bca8f0176202cdcc1ececa5112d353f8205
che-ri <rnjs1135@gmail.com>
2021-07-04 14:23:12 +0900

    [2-5] 로그인버튼 누르면 쿠키 저장하기

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,25 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-import {Text, Grid} from "./index";
+import { Text, Grid } from "./index";
 
-const Input = (props) => {
-    const {label, placeholder, _onChange} = props;
+const Input = props => {
+    const { label, placeholder, _onChange } = props;
     return (
-      <React.Fragment>
-        <Grid>
-          <Text margin="0px">{label}</Text>
-          <ElInput placeholder={placeholder} onChange={_onChange} />
-        </Grid>
-      </React.Fragment>
+        <React.Fragment>
+            <Grid>
+                <Text margin="0px">{label}</Text>
+                {/* onChange가 되면 _onChange라는 함수를 넘겨준다. */}
+                <ElInput placeholder={placeholder} onChange={_onChange} />
+            </Grid>
+        </React.Fragment>
     );
-}
+};
 
 Input.defaultProps = {
-    label: '텍스트',
-    placeholder: '텍스트를 입력해주세요.',
-    _onChange: () => {}
-}
+    //props가 없을때 오류 방지
+    label: "텍스트",
+    placeholder: "텍스트를 입력해주세요.",
+    _onChange: () => {},
+};
 
 const ElInput = styled.input`
     border: 1px solid #212121;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,45 @@
 import React from "react";
 import { Text, Input, Grid, Button } from "../elements";
+import "../shared/Cookie.js";
+import { getCookie, setCookie, deleteCookie } from "../shared/Cookie.js";
 
 const Login = props => {
+    const [id, setId] = React.useState("");
+    const [pwd, setPwd] = React.useState("");
+
+    const getId = e => {
+        setId(e.target.value);
+    };
+    const getpwd = e => {
+        setPwd(e.target.value);
+    };
+    const login = () => {
+        setCookie("user_id", id, 3);
+        setCookie("user_pwd", pwd, 3);
+        console.log(getCookie("user_id"));
+    };
+
     return (
         <React.Fragment>
             <Grid padding="16px">
                 <Text size="32px" bold>
                     로그인
                 </Text>
-
                 <Grid padding="16px 0px">
                     <Input
                         label="아이디"
                         placeholder="아이디를 입력해주세요."
-                        _onChange={() => {
-                            console.log("아이디 입력했어!");
-                        }}
+                        _onChange={getId}
                     />
                 </Grid>
-
                 <Grid padding="16px 0px">
                     <Input
                         label="패스워드"
                         placeholder="패스워드 입력해주세요."
-                        _onChange={() => {
-                            console.log("패스워드 입력했어!");
-                        }}
+                        _onChange={getpwd}
                     />
                 </Grid>
-
-                <Button
-                    text="로그인하기"
-                    _onClick={() => {
-                        console.log("로그인 했어!");
-                    }}
-                ></Button>
+                <Button text="로그인하기" _onClick={login}></Button>
             </Grid>
         </React.Fragment>
     );
