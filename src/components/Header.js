e01418d03bbcc4eedf557dda1d6538ba2b653ee6
che-ri <rnjs1135@gmail.com>
2021-07-05 00:57:42 +0900

    [2-6] 로그인하기 (1) - 헤더 분기하고 리덕스 설치하기
    - 토큰이 삭제되면 헤더모양 바꾸기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Text, Button } from "../elements";
+import { getCookie, deleteCookie } from "../shared/Cookie";
 
 const Header = props => {
+    console.log(props);
+    const [is_login, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        let cookie = getCookie("user_id");
+        cookie ? setIsLogin(true) : setIsLogin(false);
+    }, [is_login]);
+
+    if (is_login) {
+        return (
+            <React.Fragment>
+                <Grid is_flex padding="4px 16px">
+                    <Grid>
+                        <Text margin="0px" size="24px" bold>
+                            헬로
+                        </Text>
+                    </Grid>
+
+                    <Grid is_flex>
+                        <Button text="내정보"></Button>
+                        <Button text="알림"></Button>
+                        <Button
+                            text="로그아웃"
+                            _onClick={() => {
+                                deleteCookie("user_id");
+                                setIsLogin(false);
+                            }}
+                        ></Button>
+                    </Grid>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
     return (
         <React.Fragment>
             <Grid is_flex padding="4px 16px">
