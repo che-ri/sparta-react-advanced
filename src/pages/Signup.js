b85d4bfc4982d313a3c59a78989404acac600514
che-ri <rnjs1135@gmail.com>
2021-07-05 10:48:15 +0900

    [2-11] 회원가입 구현하기
    - input의 값 받아와서 state에 저장하기
    - firebase auth 이용하여 회원가입하기
    - 회원가입 예외처리 하기
    - 회원가입 후, home으로 돌아기

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,58 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Text, Input, Button } from "../elements";
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
 
-const Signup = (props) => {
-  return (
-    <React.Fragment>
-      <Grid padding="16px">
-        <Text size="32px" bold>
-          회원가입
-        </Text>
+const Signup = props => {
+    const dispatch = useDispatch();
 
-        <Grid padding="16px 0px">
-          <Input
-            label="아이디"
-            placeholder="아이디를 입력해주세요."
-            _onChange={() => {
-              console.log("!!");
-            }}
-          />
-        </Grid>
+    const signup = () => {
+        //예외처리
+        if (id === "" || pwd === "" || user_name === "")
+            alert("다시 입력해주세요!");
+        if (!pwd === pwd_check) return;
+        if (pwd === pwd_check)
+            dispatch(userActions.signupFB(id, pwd, user_name));
+    };
 
-        <Grid padding="16px 0px">
-          <Input
-            label="닉네임"
-            placeholder="닉네임을 입력해주세요."
-            _onChange={() => {
-              console.log("!!");
-            }}
-          />
-        </Grid>
+    //state로 값 저장하기
+    const [id, setId] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [pwd_check, setPwdCheck] = useState("");
+    const [user_name, setUserName] = useState("");
 
-        <Grid padding="16px 0px">
-          <Input
-            label="비밀번호"
-            placeholder="비밀번호를 입력해주세요."
-            _onChange={() => {
-              console.log("!!");
-            }}
-          />
-        </Grid>
+    return (
+        <React.Fragment>
+            <Grid padding="16px">
+                <Text size="32px" bold>
+                    회원가입
+                </Text>
 
-        <Grid padding="16px 0px">
-          <Input
-            label="비밀번호 확인"
-            placeholder="비밀번호를 다시 입력해주세요."
-            _onChange={() => {
-              console.log("!!");
-            }}
-          />
-        </Grid>
+                <Grid padding="16px 0px">
+                    <Input
+                        label="아이디"
+                        placeholder="아이디를 입력해주세요."
+                        _onChange={e => {
+                            setId(e.target.value);
+                        }}
+                    />
+                </Grid>
 
-        <Button text="회원가입하기"></Button>
-      </Grid>
-    </React.Fragment>
-  );
+                <Grid padding="16px 0px">
+                    <Input
+                        label="닉네임"
+                        placeholder="닉네임을 입력해주세요."
+                        _onChange={e => {
+                            setUserName(e.target.value);
+                        }}
+                    />
+                </Grid>
+
+                <Grid padding="16px 0px">
+                    <Input
+                        label="비밀번호"
+                        placeholder="비밀번호를 입력해주세요."
+                        _onChange={e => {
+                            setPwd(e.target.value);
+                        }}
+                    />
+                </Grid>
+
+                <Grid padding="16px 0px">
+                    <Input
+                        label="비밀번호 확인"
+                        placeholder="비밀번호를 다시 입력해주세요."
+                        _onChange={e => {
+                            setPwdCheck(e.target.value);
+                        }}
+                    />
+                </Grid>
+
+                <Button text="회원가입하기" _onClick={signup}></Button>
+            </Grid>
+        </React.Fragment>
+    );
 };
 
 Signup.defaultProps = {};
