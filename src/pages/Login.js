5f5955758002e3dc9db33a7702876dc6b39d98c7
che-ri <rnjs1135@gmail.com>
2021-07-05 16:18:11 +0900

    [error]Second argument \"password\" must be a valid string.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 //리덕스
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as userActions } from "../redux/modules/user"; //액션생성함수들을 묶어두었던 actionCreators를 불러옵니다.
+
+//firebase auth
+import { auth } from "../shared/firebase";
 
 //컴포넌트
 import { Text, Input, Grid, Button } from "../elements";
@@ -10,19 +13,17 @@ import { Text, Input, Grid, Button } from "../elements";
 //함수불러오기
 import { getCookie, setCookie, deleteCookie } from "../shared/Cookie.js";
 
-const Login = props => {
+const Login = (props) => {
     const dispatch = useDispatch();
-    const [id, setId] = React.useState("");
-    const [pwd, setPwd] = React.useState("");
+    const [id, setId] = useState("");
+    const [pwd, setPwd] = useState("");
 
-    const getId = e => {
-        setId(e.target.value);
-    };
-    const getpwd = e => {
-        setPwd(e.target.value);
-    };
     const login = () => {
-        dispatch(userActions.loginAction({ user_id: "cheri" }));
+        if (id === "" || pwd === "") {
+            window.alert("아이디 혹은 비밀번호가 공란입니다! 입력해주세요!");
+            return;
+        }
+        dispatch(userActions.loginFB((id, pwd)));
     };
 
     return (
@@ -35,14 +36,14 @@ const Login = props => {
                     <Input
                         label="아이디"
                         placeholder="아이디를 입력해주세요."
-                        _onChange={getId}
+                        _onChange={(e) => setId(e.target.value)}
                     />
                 </Grid>
                 <Grid padding="16px 0px">
                     <Input
                         label="패스워드"
                         placeholder="패스워드 입력해주세요."
-                        _onChange={getpwd}
+                        _onChange={(e) => setPwd(e.target.value)}
                     />
                 </Grid>
                 <Button
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -9,18 +9,18 @@ import { setCookie, getCookie, deleteCookie } from "../../shared/Cookie";
 
 //auth 가져오기
 import { auth } from "../../shared/firebase";
+import firebase from "firebase/app";
 
 // action : 액션 타입 만들기
-const SET_USER = "SET_USER";
-const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
+const SET_USER = "SET_USER";
 const GET_USER = "GET_USER";
 
 // action creators : redux-actions 사용
 // createAction(액션타입, (params)=>({return할 내용}))
-const setUser = createAction(SET_USER, user => ({ user }));
-const logOut = createAction(LOG_OUT, user => ({ user }));
-const getUser = createAction(GET_USER, user => ({ user }));
+const logOut = createAction(LOG_OUT, (user) => ({ user }));
+const setUser = createAction(SET_USER, (user) => ({ user }));
+const getUser = createAction(GET_USER, (user) => ({ user }));
 
 //initialState
 const initialState = {
@@ -29,13 +29,35 @@ const initialState = {
 };
 
 // middlewware actions
-const loginAction = user => {
-    //state 받아서 가져오는 것을 getState로 할 수 있었습니다.
-    //history는 configureStore.js에서 withExtraArgument으로 history를 넣어주었던 것이지요.
+// const loginAction = user => {
+//     //state 받아서 가져오는 것을 getState로 할 수 있었습니다.
+//     //history는 configureStore.js에서 withExtraArgument으로 history를 넣어주었던 것이지요.
+//     return function (dispatch, getState, { history }) {
+//         dispatch(setUser(user));
+//         history.push("/");
+//         //코드를 보다싶이 리덕스액션이 실행되고 나서 history를 통하여 페이지 이동할 수 있겠죠!
+//     };
+// };
+
+const loginFB = (id, pwd) => {
     return function (dispatch, getState, { history }) {
-        dispatch(setUser(user));
-        history.push("/");
-        //코드를 보다싶이 리덕스액션이 실행되고 나서 history를 통하여 페이지 이동할 수 있겠죠!
+        auth.signInWithEmailAndPassword(id, pwd)
+            .then((user) => {
+                console.log(user);
+                dispatch(
+                    setUser({
+                        user_name: user.user.displayName,
+                        id: id,
+                        user_profile: "",
+                    })
+                );
+                history.push("/");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
     };
 };
 
@@ -44,7 +66,7 @@ const signupFB = (id, pwd, user_name) => {
         //비밀번호 기반 회원가입 https://firebase.google.com/docs/auth/web/password-auth?authuser=0
         //email과 password 정보로 가입합니다.
         auth.createUserWithEmailAndPassword(id, pwd)
-            .then(user => {
+            .then((user) => {
                 // 유저 프로필 업데이트 https://firebase.google.com/docs/auth/web/manage-users?authuser=0
                 // 우리는 유저네임도 넣어야하기때문에, 가입성공 후 유저네임을 업.데.이.트 하는 방식으로 할 것입니다.
                 auth.currentUser
@@ -52,12 +74,18 @@ const signupFB = (id, pwd, user_name) => {
                         displayName: user_name,
                     })
                     .then(() => {
-                        dispatch(setUser({ user_name, id, user_profile: "" }));
+                        dispatch(
+                            setUser({
+                                user_name: user_name,
+                                id: id,
+                                user_profile: "",
+                            })
+                        );
                         history.push("/");
                     })
-                    .catch(error => console.log(error));
+                    .catch((error) => console.log(error));
             })
-            .catch(error => {
+            .catch((error) => {
                 let errorCode = error.code;
                 let errorMessage = error.message;
                 console.log(errorCode, errorMessage);
@@ -72,14 +100,14 @@ export default handleActions(
         [SET_USER]: (state, action) =>
             //return 할 내용
             //produce(원본값, 복사한 원본값으로 할 작업)
-            produce(state, draft => {
+            produce(state, (draft) => {
                 setCookie("is_login", "success");
                 //payload에 우리가 보는 데이터가 담깁니다.
                 draft.user = action.payload.user;
                 draft.is_login = true;
             }),
         [LOG_OUT]: (state, action) => {
-            produce(state, draft => {
+            produce(state, (draft) => {
                 deleteCookie("is_login"); //쿠키를 삭제해봅니다.
                 draft.user = null;
                 draft.is_login = false;
@@ -91,5 +119,5 @@ export default handleActions(
 ); //initialState자리는 state 초기값입니다.
 
 //action creator export
-const actionCreators = { logOut, getUser, loginAction, signupFB };
+const actionCreators = { loginFB, logOut, getUser, signupFB };
 export { actionCreators };
